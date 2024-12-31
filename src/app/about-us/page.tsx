0ae25fcd8d011d9ed3a6988c41848ac65f235250
Sohrab14 <sohrabmkhan@acpce.ac.in>
2024-12-31 16:22:58 +0530

    first commit

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -4,15 +4,7 @@ import { Stats } from "@/components/about-us/stats";
 import { Values } from "@/components/about-us/values";
 import { WorldWide } from "@/components/about-us/world-wide";
 
-import { CustomerJourney } from "@/components/customer-journey";
-
-import { CustomerCrmNext } from "@/components/ui/custom/product/crmnext/customer-crm-next";
-import { DashboardReport } from "@/components/ui/custom/product/crmnext/dashboard-report";
-import { InsightCrmNext } from "@/components/ui/custom/product/crmnext/insight-crm-next";
-import { LeaderCrmNext } from "@/components/ui/custom/product/crmnext/leader-crm-next";
 import { Personalization } from "@/components/ui/custom/product/crmnext/personalization";
-
-import { GptNextBanner } from "@/components/ui/custom/product/gpt-next/gpt-next-banner";
 
 export default function page() {
   return (
@@ -23,12 +15,6 @@ export default function page() {
       <Stats />
       <Personalization />
       <WorldWide />
-      {/* <GptNextBanner />
-      <CustomerJourney />
-      <DashboardReport />
-      <LeaderCrmNext />
-      <InsightCrmNext />
-      <CustomerCrmNext /> */}
     </>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import { Banner } from "@/components/banner";
-import { Banner2 } from "@/components/ui/custom/product/crmnext/crm-next-banner";
+
 import { PartnershipBrand } from "@/components/brand";
 
 import { EcoSystem } from "@/components/eco-system";
 import { ExperienceLayer } from "@/components/experience-layer";
-import { Footer } from "@/components/footer";
+
 import { Insight } from "@/components/insight";
 
 import Navbar from "@/components/navbar";
